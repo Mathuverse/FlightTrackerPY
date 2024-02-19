0209fb79132d845549a5626016791b73016ac06a
Mathuverse <Mathusan54@gmail.com>
2024-02-19 09:27:09 -0500

    NotificationManager Done

--- notificationManager.py
+++ notificationManager.py
@@ -5,4 +5,13 @@ PASS = "ngzjeqhpuqfwbqja"
 
 class NotificationManager:
 
+    #Fonction from which we will send the user the emails for the flight prices
     def sendEmail(self,msg):
+        with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASS)
+            connection.sendmail(
+                from_addr=MY_EMAIL,
+                to_addrs="Mathusan54@gmail.com",
+                msg=msg
+            )
